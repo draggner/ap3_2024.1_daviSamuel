1e25bead253dccf74a34d23392d39f86780a5db4
Davi Samuel <devdraggnerstudio@gmail.com>
2024-08-02 21:53:53 -0300

    AP3 - U1 2024-08-02 Aula 04 - Viabilidade de Atributos

---
+++ unidade1/gabarito_aula4/TestaListaAlunos.java
@@ -0,0 +1,22 @@
+package unidade1.gabarito_aula4;
+
+import java.util.LinkedList;
+
+public class TestaListaAlunos {
+  public static void main(String[] args) {    
+    LinkedList<Aluno> osAlunos = new LinkedList<Aluno>();
+    for (int i = 0; i < 89764; i++) {
+      Aluno umAluno = new Aluno();
+
+      umAluno.setNome("AlunoNumero" + i);
+      osAlunos.add(umAluno);
+
+    }
+
+    System.out.println("Imprimindo a Lista Alunos");
+
+    for (int i = 0; i < osAlunos.size(); i++) {
+      System.out.println(osAlunos.get(i));
+    }
+  }
+}

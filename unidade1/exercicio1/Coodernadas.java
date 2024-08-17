0ef11366923973f8568cdbf1a8133d0d93777195
Davi Samuel <devdraggnerstudio@gmail.com>
2024-08-17 00:44:47 -0300

    AP3 U1 2024-08-17 Correção na fórmula da distância

--- unidade1/exercicio1/Coodernadas.java
+++ unidade1/exercicio1/Coodernadas.java
@@ -9,11 +9,11 @@ import unidade1.exercicio1.Questao1;
  * 
  * Este programa demonstra a inicialização de inserir valores de coordenadas especificas para descobrir a distância e amplitude.
  * 
- * @author davi
- * @version 1.0
- * @since 2024-08-14
+ * @author Davi Samuel G. A.
+ * @version 1.1
+ * @since 2024-08-17
  * 
- * 695686780
+ * COD: 695686780
 */
 
 public class Coodernadas {
@@ -25,26 +25,27 @@ public class Coodernadas {
     System.out.println("Informe as Coordenadas de X e Y nesta respectiva ordem: ");
 
     System.out.println("Valor de X1: ");
-    double x1 = teclado.nextFloat();
+    double x1 = teclado.nextDouble();
     umQuestao1.setX1(x1);
     System.out.println(umQuestao1.getX1());
 
     System.out.println("Valor de X2: ");
-    double x2 = teclado.nextFloat();
+    double x2 = teclado.nextDouble();
     umQuestao1.setX2(x2);
     System.out.println(umQuestao1.getX2());
 
     System.out.println("Valor de Y1: ");
-    double y1 = teclado.nextFloat();
+    double y1 = teclado.nextDouble();
     umQuestao1.setY1(y1);
     System.out.println(umQuestao1.getY1());
 
     System.out.println("Valor de Y2: ");
-    double y2 = teclado.nextFloat();
+    double y2 = teclado.nextDouble();
     umQuestao1.setY2(y2);
     System.out.println(umQuestao1.getY2());
 
-    umQuestao1.distancia = Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), 2);
+    double distancia = umQuestao1.distancia = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    umQuestao1.setDistancia(distancia);
     System.out.println("Valor Total da Distância: " + umQuestao1.getDistancia());
 
     // Dar continuidade a parti daqui
